6220190003863f115c6eb3de01ef149a733050f5
Ivan Fišić <ivfisic@vsite.hr>
2024-05-27 17:57:32 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace GmailApiTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonAddAttachment = new System.Windows.Forms.Button();
+            this.textBoxAttachment = new System.Windows.Forms.TextBox();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.textBoxCC = new System.Windows.Forms.TextBox();
+            this.richTextBoxMessage = new System.Windows.Forms.RichTextBox();
+            this.buttonSendEmail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTo.Location = new System.Drawing.Point(320, 49);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(290, 30);
+            this.textBoxTo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(272, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "To:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(266, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CC:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(230, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Subject:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Message:";
+            // 
+            // buttonAddAttachment
+            // 
+            this.buttonAddAttachment.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonAddAttachment.Location = new System.Drawing.Point(149, 406);
+            this.buttonAddAttachment.Name = "buttonAddAttachment";
+            this.buttonAddAttachment.Size = new System.Drawing.Size(119, 43);
+            this.buttonAddAttachment.TabIndex = 8;
+            this.buttonAddAttachment.Text = "Add Attachment";
+            this.buttonAddAttachment.UseCompatibleTextRendering = true;
+            this.buttonAddAttachment.UseVisualStyleBackColor = true;
+            this.buttonAddAttachment.Click += new System.EventHandler(this.buttonAddAttachment_Click);
+            // 
+            // textBoxAttachment
+            // 
+            this.textBoxAttachment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAttachment.Location = new System.Drawing.Point(149, 370);
+            this.textBoxAttachment.Name = "textBoxAttachment";
+            this.textBoxAttachment.Size = new System.Drawing.Size(359, 30);
+            this.textBoxAttachment.TabIndex = 10;
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSubject.Location = new System.Drawing.Point(320, 121);
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(290, 30);
+            this.textBoxSubject.TabIndex = 11;
+            // 
+            // textBoxCC
+            // 
+            this.textBoxCC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCC.Location = new System.Drawing.Point(320, 85);
+            this.textBoxCC.Name = "textBoxCC";
+            this.textBoxCC.Size = new System.Drawing.Size(290, 30);
+            this.textBoxCC.TabIndex = 12;
+            // 
+            // richTextBoxMessage
+            // 
+            this.richTextBoxMessage.Location = new System.Drawing.Point(151, 164);
+            this.richTextBoxMessage.Name = "richTextBoxMessage";
+            this.richTextBoxMessage.Size = new System.Drawing.Size(459, 200);
+            this.richTextBoxMessage.TabIndex = 13;
+            this.richTextBoxMessage.Text = "";
+            // 
+            // buttonSendEmail
+            // 
+            this.buttonSendEmail.Location = new System.Drawing.Point(514, 370);
+            this.buttonSendEmail.Name = "buttonSendEmail";
+            this.buttonSendEmail.Size = new System.Drawing.Size(96, 79);
+            this.buttonSendEmail.TabIndex = 14;
+            this.buttonSendEmail.Text = "Send email";
+            this.buttonSendEmail.UseVisualStyleBackColor = true;
+            this.buttonSendEmail.Click += new System.EventHandler(this.buttonSendEmail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 500);
+            this.Controls.Add(this.buttonSendEmail);
+            this.Controls.Add(this.richTextBoxMessage);
+            this.Controls.Add(this.textBoxCC);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.textBoxAttachment);
+            this.Controls.Add(this.buttonAddAttachment);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonAddAttachment;
+        private System.Windows.Forms.TextBox textBoxAttachment;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.TextBox textBoxCC;
+        private System.Windows.Forms.RichTextBox richTextBoxMessage;
+        private System.Windows.Forms.Button buttonSendEmail;
+    }
+}
+
